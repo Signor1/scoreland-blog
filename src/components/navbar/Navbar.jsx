fbe630b052298cdf927019b51a49bf1cf3e049fc
signor1 <emmanuelomemgboji@gmail.com>
2023-11-28 11:52:24 +0100

    chore: added resp navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Link, NavLink } from "react-router-dom"
+import { NavLinks } from "../utils/NavLinks"
+import { useState } from "react"
+import { List, X } from "@phosphor-icons/react"
+
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleSideBarToggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <header className="w-full relative font-sans h-20 flex justify-between lg:px-16 px-4 items-center bg-gray-950 border-b-2 border-purple-600">
+            <Link to="/" className="inline-block uppercase text-gray-50">ScoreLand</Link>
+            <ul className="md:flex hidden gap-8 font-sans">
+                {
+                    NavLinks.map((navlink, index) => (
+                        <li key={index}>
+                            <NavLink to={navlink.url} className="text-sm text-gray-50 relative inline-block before:absolute before:bottom-0 before:left-0 before:w-0 before:transition-all before:duration-200 hover:before:w-full before:h-0.5 before:bg-purple-600">{navlink.name}</NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+            <button onClick={handleSideBarToggle} className="md:hidden inline-block text-white bg-purple-600 px-3 py-2 rounded" type="button">
+                <List size={20} />
+            </button>
+
+            {/* Mobile side nav */}
+            <div className={`flex md:hidden flex-col absolute top-0 transition-all duration-300 w-full z-50 h-screen bg-gray-950 ${isOpen ? "left-0" : "left-[100%]"}`}>
+                <header className="w-full border-b border-purple-600 flex justify-between items-center py-6 px-5">
+                    <Link to="/" className="inline-block uppercase text-gray-50">ScoreLand</Link>
+                    <button onClick={handleSideBarToggle} className="inline-block text-white bg-purple-600 px-3 py-2 rounded" type="button">
+                        <X size={20} />
+                    </button>
+                </header>
+                <ul className="flex flex-col mt-10 px-6 gap-6 font-sans">
+                    {
+                        NavLinks.map((navlink, index) => (
+                            <li key={index}>
+                                <NavLink to={navlink.url} onClick={handleSideBarToggle} className="text-sm text-gray-50 relative inline-block before:absolute before:bottom-0 before:left-0 before:w-0 before:transition-all before:duration-200 hover:before:w-full before:h-0.5 before:bg-purple-600">{navlink.name}</NavLink>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
---
+++ src/components/utils/NavLinks.js
@@ -0,0 +1,22 @@
+export const NavLinks = [
+  {
+    name: "Latest News",
+    url: "/latestnews",
+  },
+  {
+    name: "Transfers",
+    url: "/transfers",
+  },
+  {
+    name: "Premier League",
+    url: "/premierleague",
+  },
+  {
+    name: "La Liga",
+    url: "/laliga",
+  },
+  {
+    name: "Serie A",
+    url: "/seriea",
+  },
+];
